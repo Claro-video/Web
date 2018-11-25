9228ef36744b8e33227db75765beb4f7545535f2
vpsradio <45034066+vpsradio@users.noreply.github.com>
2018-11-25 19:51:18 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,28 +1,34 @@
-<?php 
-$urlkey = @$_GET["urlkey"];
-$time = @$_GET["time"];
+<?
+$id = @$_GET['id'];
+$numbanners = 1; //numero de banners que se rotarán
+$random = rand(1,$numbanners);
+$url = array();
+ 
+$url[1] = "http://claro1.pi-dns.tk/C1a4o/" . $id . "/index.m3u8";
 
-if($urlkey == 'ESPN_2_HD'){
-	$canal2 = "https://claro-video.github.io/dat/fe/" . $urlkey . ".dat";
-	$fp=fopen("$canal2", "r");
-	fpassthru($fp);
-   }else{
-if($urlkey == 'ESPN_3_HD'){
-$canal2 = "https://claro-video.github.io/dat/fk/" . $urlkey . ".dat";
-    $fp=fopen("$canal2", "r");
-    fpassthru($fp);
-   }else{
-if($urlkey == 'ESTRELLA_HD'){
-	$canal2 = "https://claro-video.github.io/dat/fk/" . $urlkey . ".dat";
-	$fp=fopen("$canal2", "r");
-	fpassthru($fp);
-   }else{
-	$canal2 = "https://claro-video.github.io/dat/kr/" . $urlkey . "." . $time . ".dat";
-	$fp=fopen("$canal2", "r");
-	fpassthru($fp);
-   }
-   }
-   }
+$url[2] = "http://claro2.pi-dns.tk/C1a4o/" . $id . "/index.m3u8";
+
+$url[3] = "http://claro3.pi-dns.tk/C1a4o/" . $id . "/index.m3u8";
+
+$url[4] = "http://claro4.pi-dns.tk/C1a4o/" . $id . "/index.m3u8";
+
+$url[5] = "http://claro5.pi-dns.tk/C1a4o/" . $id . "/index.m3u8";
+
+$url[6] = "http://claro6.pi-dns.tk/C1a4o/" . $id . "/index.m3u8";
+
+$url[7] = "http://claro7.pi-dns.tk/C1a4o/" . $id . "/index.m3u8";
+
+$url[8] = "http://claro8.pi-dns.tk/C1a4o/" . $id . "/index.m3u8";
+
+$url[9] = "http://claro9.pi-dns.tk/C1a4o/" . $id . "/index.m3u8";
 
 
-     ?>
+
+ 
+$canal2 = $url[$random];
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+header("Content-type: application/vnd.apple.mpegurl");
+header("Content-Disposition: attachment; filename=$id.m3u8");
+
+?>
