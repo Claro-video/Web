0fed01b5c9f872f31db756cf0705bcae788aaa9d
vpsradio <45034066+vpsradio@users.noreply.github.com>
2018-11-26 10:31:16 +0100

    Update canal.php

--- canal.php
+++ canal.php
@@ -1,7 +1,7 @@
 <?
 $id = @$_GET['id'];
 $numbanners = 4; //numero de banners que se rotarán
-$random = rand(2,$numbanners);
+$random = rand(1,$numbanners);
 $url = array();
  
 $url[1] = "http://claro.pi-dns.tk/C1a4o/" . $id . "/index.m3u8";
