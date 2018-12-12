627ac700d5092bf968cbd0d9b463d37d7071ad6d
vpsradio <45034066+vpsradio@users.noreply.github.com>
2018-12-12 18:27:57 +0100

    Update Canal.php

--- Canal.php
+++ Canal.php
@@ -3,8 +3,6 @@ $server_anfitrion = "http://" . $_SERVER['SERVER_NAME'];
 $id = @$_GET['id'];
 header("Content-type: application/vnd.apple.mpegurl");
 header("Content-Disposition: attachment; filename=$id.m3u8");
-
-
 if($id == 'ESPN_2_HD'){
 $canal2 = $server_anfitrion . "/02/" . $id . "/4.m3u8";
 $obtener3 = file_get_contents($canal2);
@@ -25,6 +23,31 @@ $canal2 = $server_anfitrion . "/02/" . $id . "/4.m3u8";
 $obtener3 = file_get_contents($canal2);
 echo $obtener3;
    }else{
+if($id == 'RCN_HD'){
+$canal2 = $server_anfitrion . "/rcn.php?id=" . $id;
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+    }else{
+if($id == 'RCN_HD2'){
+$canal2 = $server_anfitrion . "/rcn.php?id=" . $id;
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+    }else{
+if($id == 'TACHO_PISTACHO'){
+$canal2 = $server_anfitrion . "/rcn.php?id=" . $id;
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+    }else{
+if($id == 'SENAL_COLOMBIA'){
+$canal2 = $server_anfitrion . "/rcn.php?id=" . $id;
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+    }else{
+if($id == 'CANAL_INSTITUCIONAL'){
+$canal2 = $server_anfitrion . "/rcn.php?id=" . $id;
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+    }else{
 $canal2 = $server_anfitrion . "/01/" . $id . "/4.m3u8";
 $obtener3 = file_get_contents($canal2);
 echo $obtener3;
@@ -32,6 +55,9 @@ echo $obtener3;
    }
    }
    }
-
-
-     ?>
+   }
+   }
+   }
+   }
+   }  
+?>
