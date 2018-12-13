b1557a58a90166fee8b8ffbab19696e7de20b06b
vpsradio <45034066+vpsradio@users.noreply.github.com>
2018-12-13 12:36:04 +0100

    Update C@nal.php

--- C@nal.php
+++ C@nal.php
@@ -1,1 +1,102 @@
-<?php $_F=__FILE__;$_X='Pz48P3BocA0KJDRkID0gQCRfR0VUWyc0ZCddOw0KDQo0ZigkNGQgPT0gJ1JDTl9IRCcpew0KJDFsZjRuID0gImh0dHBzOi8vcmNuaGQuY2RuLm41dC5jMi9sNHY1MDYvcmNuaGRfN2EwcC5tbzM4IjsNCiQxbGY0bmEgPSAiaHR0cHM6Ly9yY25oZC5jZG4ubjV0LmMyL2w0djUwNiI7DQokZjRsNSA9IGYycDVuICgiJDFsZjRuIiwgInIiKTsNCndoNGw1ICghZjUyZiAoJGY0bDUpKSB7DQogICAgJGw0bjUgPSBmZzV0cyAoJGY0bDUsIDYwYXUpOw0KICAgIDRmICgkbDRuNVswXSA9PSAiIyIpIHsNCgkgNWNoMiAkbDRuNSAuICAiXG4iOw0KICAgIH0gNWxzNSB7DQogICAgICAgIDVjaDIgIiQxbGY0bmEvIi4kbDRuNSAuICJcbiI7DQogICAgfQ0KDQp9DQpmY2wyczUoJGY0bDUpOw0KDQogICB9NWxzNXsNCjRmKCQ0ZCA9PSAnUkNOX0hEYScpew0KJDFsZjRuID0gImh0dHBzOi8vcmNuaGQuY2RuLm41dC5jMi9sNHY1MGEvcmNuaGRhXzdhMHAubW8zOCI7DQokMWxmNG5hID0gImh0dHBzOi8vcmNuaGQuY2RuLm41dC5jMi9sNHY1MGEiOw0KJGY0bDUgPSBmMnA1biAoIiQxbGY0biIsICJyIik7DQp3aDRsNSAoIWY1MmYgKCRmNGw1KSkgew0KICAgICRsNG41ID0gZmc1dHMgKCRmNGw1LCA2MGF1KTsNCiAgICA0ZiAoJGw0bjVbMF0gPT0gIiMiKSB7DQoJIDVjaDIgJGw0bjUgLiAgIlxuIjsNCiAgICB9IDVsczUgew0KICAgICAgICA1Y2gyICIkMWxmNG5hLyIuJGw0bjUgLiAiXG4iOw0KICAgIH0NCg0KfQ0KZmNsMnM1KCRmNGw1KTsNCiAgIH01bHM1ew0KNGYoJDRkID09ICdUQUNIT19QSVNUQUNITycpew0KJDFsZjRuID0gImh0dHBzOi8vcmNuaGQuY2RuLm41dC5jMi9sNHY1MG8vdDFjaDJfdTgwcC5tbzM4IjsNCiQxbGY0bmEgPSAiaHR0cHM6Ly9yY25oZC5jZG4ubjV0LmMyL2w0djUwbyI7DQokZjRsNSA9IGYycDVuICgiJDFsZjRuIiwgInIiKTsNCndoNGw1ICghZjUyZiAoJGY0bDUpKSB7DQogICAgJGw0bjUgPSBmZzV0cyAoJGY0bDUsIDYwYXUpOw0KICAgIDRmICgkbDRuNVswXSA9PSAiIyIpIHsNCgkgNWNoMiAkbDRuNSAuICAiXG4iOw0KICAgIH0gNWxzNSB7DQogICAgICAgIDVjaDIgIiQxbGY0bmEvIi4kbDRuNSAuICJcbiI7DQogICAgfQ0KDQp9DQpmY2wyczUoJGY0bDUpOw0KDQogICB9NWxzNXsNCjRmKCQ0ZCA9PSAnU0VOQUxfQ09MT01CSUEnKXsNCiQxbGY0biA9ICJodHRwOi8vbDR2NTYwc3NsLmNkbm01ZDQxLnR2L3J0dmNzYzJsMm1iNDFsNHY1L3NtNGw6bDR2NS5zbTRsLy5tbzM4IjsNCiQxbGY0bmEgPSAiaHR0cDovL2w0djU2MHNzbC5jZG5tNWQ0MS50di9ydHZjc2MybDJtYjQxbDR2NS9zbTRsOmw0djUuc200bCI7DQokZjRsNSA9IGYycDVuICgiJDFsZjRuIiwgInIiKTsNCndoNGw1ICghZjUyZiAoJGY0bDUpKSB7DQogICAgJGw0bjUgPSBmZzV0cyAoJGY0bDUsIDYwYXUpOw0KICAgIDRmICgkbDRuNVswXSA9PSAiIyIpIHsNCgkgNWNoMiAkbDRuNSAuICAiXG4iOw0KICAgIH0gNWxzNSB7DQogICAgICAgIDVjaDIgIiQxbGY0bmEvIi4kbDRuNSAuICJcbiI7DQogICAgfQ0KDQp9DQpmY2wyczUoJGY0bDUpOw0KICAgfQ0KNWxzNXsNCjRmKCQ0ZCA9PSAnQ0FOQUxfSU5TVElUVUNJT05BTCcpew0KJDFsZjRuID0gImh0dHA6Ly9sNHY1NjBzc2wuY2RubTVkNDEudHYvcnR2Y3M0bnN0NHQzYzQybjFsbDR2NS9zbTRsOmw0djVhLnNtNGwvLm1vMzgiOw0KJDFsZjRuYSA9ICJodHRwOi8vbDR2NTYwc3NsLmNkbm01ZDQxLnR2L3J0dmNzNG5zdDR0M2M0Mm4xbGw0djUvc200bDpsNHY1YS5zbTRsIjsNCiRmNGw1ID0gZjJwNW4gKCIkMWxmNG4iLCAiciIpOw0Kd2g0bDUgKCFmNTJmICgkZjRsNSkpIHsNCiAgICAkbDRuNSA9IGZnNXRzICgkZjRsNSwgNjBhdSk7DQogICAgNGYgKCRsNG41WzBdID09ICIjIikgew0KCSA1Y2gyICRsNG41IC4gICJcbiI7DQogICAgfSA1bHM1IHsNCiAgICAgICAgNWNoMiAiJDFsZjRuYS8iLiRsNG41IC4gIlxuIjsNCiAgICB9DQoNCn0NCmZjbDJzNSgkZjRsNSk7DQogICB9DQo1bHM1ew0KNGYoJDRkID09ICdURUxFQ0FSSUJFX0RFUE9SVEVTJyl7DQokMWxmNG4gPSAiaHR0cHM6Ly90NWw1YzFyNGI1LWQ1cDJydDVzLmdjZG4uMW52MXQyLm41dC9obHMvbTFuNGY1c3RzL3Q1bDVjMXI0YjVkNXAycnQ1c2hkYS9tMXN0NXIubW8zOD81cXA9UW1ZeFpFWlZZVGt4MWpodE5XOXhOVmt3Y1hOc1phNnpiSFIzZERWeE5vcElSbVFyVWtabGQwTk5VbWlrUkRCdVVFbHRVRVV5UjQ4dlRscHNOVUZwZGtNeldqa3pibFZFYkRsM1pEVlJjVXV6WW9kMFVGUlVWMHBKNUZwSVRYZGsxV1J0Wmt4bU9GUktTVWNhZGE5RUt6aGxMNlZzT1ZsNk4wVXVkUzl4V2twWVpFcDZOR1ZKU0daeVFrZERNMFJuMURabFpWWkMxakpqMWtpcFJIa3dkSEJPVG5SeFJHOVY1VEVvWWFrb2RvUnFaUzlTMW9FNmJhMGlZMGlDMUdGSmRFTXpOR0pqUlVGS1lqWlhSVFpIY0ZBdU5uWjRUa3N6WlRJMFQwbFRkWFIyZEYyYWNrWlliMGxGYm9CQllheDMxeTltWm1pamJGWXVZVUpMNUU2Q05vUmsxREpHYzBsRlduQk9ZNHRPVDR0dlQ2bGhOYWRSWkZSSFVHRnpjMEY0TTQ5cUs2bDZiVzlvWkdKSVM2aDZONkI2ZFV1MDE0dFFXa1pQSzBaQ0w2VmFia2R5V25aMVZrSnlaRzlQY29wRGNIVnBibGhsTG9aVlpGbDMxV2kwVkd0RTFIbHRSQ3RPVHpGWU1ETXYxSEJwWjBKWDFtaDM1VUpQUWs2RVVUSlQmMW52dHI0ZD11ZTdkMGM3YmE1MTk4YWI5Y2QxYzBvdTU5aWQwYWNmMCYxbnYxM3RoPXRiPTB+dDU9Nml1YXU4MDY5OX5zZ249ZGl1dTU4ZGNmMGE4OGYxNWlhZmZiYjhkNjA2MDA5Y281Yjg2YW9pZWI1ZmJ1YmU4NWM1ZjVpOTVvY3ViMDlpOSZ0PTZpdWF1ODA2MDkiOw0KJGY0bDUgPSBmMnA1biAoIiQxbGY0biIsICJyIik7DQp3aDRsNSAoIWY1MmYgKCRmNGw1KSkgew0KICAgICRsNG41ID0gZmc1dHMgKCRmNGw1LCA2MGF1KTsNCiAgICA0ZiAoJGw0bjVbMF0gPT0gIiMiKSB7DQoJIDVjaDIgJGw0bjUgLiAgIlxuIjsNCiAgICB9IDVsczUgew0KICAgICAgICA1Y2gyICRsNG41IC4gIlxuIjsNCiAgICB9DQoNCn0NCmZjbDJzNSgkZjRsNSk7DQogICB9DQogICB9DQogICB9DQogICB9DQogICB9DQogICB9DQo/Pg0K';eval(base64_decode('JF9YPWJhc2U2NF9kZWNvZGUoJF9YKTskX1g9c3RydHIoJF9YLCcxMjM0NTZhb3VpZScsJ2FvdWllMTIzNDU2Jyk7JF9SPWVyZWdfcmVwbGFjZSgnX19GSUxFX18nLCInIi4kX0YuIiciLCRfWCk7ZXZhbCgkX1IpOyRfUj0wOyRfWD0wOw=='));?>
+<?php
+$id = @$_GET['id'];
+
+if($id == 'RCN_HD'){
+$alfin = "https://rcnhd.cdn.net.co/live01/rcnhd_720p.m3u8";
+$alfin2 = "https://rcnhd.cdn.net.co/live01";
+$file = fopen ("$alfin", "r");
+while (!feof ($file)) {
+    $line = fgets ($file, 1024);
+    if ($line[0] == "#") {
+	 echo $line .  "\n";
+    } else {
+        echo "$alfin2/".$line . "\n";
+    }
+
+}
+fclose($file);
+
+   }else{
+if($id == 'RCN_HD2'){
+$alfin = "https://rcnhd.cdn.net.co/live02/rcnhd2_720p.m3u8";
+$alfin2 = "https://rcnhd.cdn.net.co/live02";
+$file = fopen ("$alfin", "r");
+while (!feof ($file)) {
+    $line = fgets ($file, 1024);
+    if ($line[0] == "#") {
+	 echo $line .  "\n";
+    } else {
+        echo "$alfin2/".$line . "\n";
+    }
+
+}
+fclose($file);
+   }else{
+if($id == 'TACHO_PISTACHO'){
+$alfin = "https://rcnhd.cdn.net.co/live03/tacho_480p.m3u8";
+$alfin2 = "https://rcnhd.cdn.net.co/live03";
+$file = fopen ("$alfin", "r");
+while (!feof ($file)) {
+    $line = fgets ($file, 1024);
+    if ($line[0] == "#") {
+	 echo $line .  "\n";
+    } else {
+        echo "$alfin2/".$line . "\n";
+    }
+
+}
+fclose($file);
+
+   }else{
+if($id == 'SENAL_COLOMBIA'){
+$alfin = "http://live10ssl.cdnmedia.tv/rtvcscolombialive/smil:live.smil/.m3u8";
+$alfin2 = "http://live10ssl.cdnmedia.tv/rtvcscolombialive/smil:live.smil";
+$file = fopen ("$alfin", "r");
+while (!feof ($file)) {
+    $line = fgets ($file, 1024);
+    if ($line[0] == "#") {
+	 echo $line .  "\n";
+    } else {
+        echo "$alfin2/".$line . "\n";
+    }
+
+}
+fclose($file);
+   }
+else{
+if($id == 'CANAL_INSTITUCIONAL'){
+$alfin = "http://live10ssl.cdnmedia.tv/rtvcsinstitucionallive/smil:live2.smil/.m3u8";
+$alfin2 = "http://live10ssl.cdnmedia.tv/rtvcsinstitucionallive/smil:live2.smil";
+$file = fopen ("$alfin", "r");
+while (!feof ($file)) {
+    $line = fgets ($file, 1024);
+    if ($line[0] == "#") {
+	 echo $line .  "\n";
+    } else {
+        echo "$alfin2/".$line . "\n";
+    }
+
+}
+fclose($file);
+   }
+else{
+if($id == 'TELECARIBE_DEPORTES'){
+$alfin = "https://telecaribe-deportes.gcdn.anvato.net/hls/manifests/telecaribedeporteshd2/master.m3u8?eqp=QmYxZEZVYTkxajhtNW9xNVkwcXNsZ21zbHRudDVxN3pIRmQrUkZld0NNUm5kRDB4UEltUEUyRi8vTlpsNUFpdkMzWjkzblVEbDluZDVRcU4zY3d0UFRUV0pJeFpITXdkaWRtZkxmOFRKSUc2d29EKzhlL1VsOVl1N0U4dS9xWkpYZEp1NGVJSGZyQkdDM0RnaDZlZVZCajJjak5pRHkwdHBOTnRxRG9VeTE3Y2k3d3RqZS9Sa3E1b205Y05CaGFJdEMzNGJjRUFKYjZXRTZHcFA4NnZiTkszZTI0T0lTdXRodFo2ckZYb0lFb3BBY2xuay9mZm5jbFY4YUJLeE1CN3RkaDJGc0lFWnBOYitOTitvT1lhN2dRZFRHUGFzc0FiMi9qK1l1bW93ZGJIS1h1N1B1dU40aitQWkZPK0ZCL1V2bkdyWnZaVkJyZG9Pc3pDcHVpblhlL3ZVZFluaW50VGtEaHltRCtOTzFYMDMvaHBpZ0JXamhueUJPQk1EUTJT&anvtrid=467d0c7b2ea982b9cdac034e95d02cf0&anvauth=tb=0~te=1542480199~sgn=d544e8dcf0288fae52ffbb8d101009c3eb812356befb4b68ecefe59e3c4b0959&t=1542480109";
+$file = fopen ("$alfin", "r");
+while (!feof ($file)) {
+    $line = fgets ($file, 1024);
+    if ($line[0] == "#") {
+	 echo $line .  "\n";
+    } else {
+        echo $line . "\n";
+    }
+
+}
+fclose($file);
+   }
+   }
+   }
+   }
+   }
+   }
+?>
