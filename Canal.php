cf7fad95a643a2b3c7a5258d116a15a5e55f2d70
vpsradio <45034066+vpsradio@users.noreply.github.com>
2018-12-13 12:36:34 +0100

    Update Canal.php

--- Canal.php
+++ Canal.php
@@ -1,1 +1,63 @@
-<?php $_F=__FILE__;$_X='Pz48P3BocCANCiRzNXJ2NXJfMW5mNHRyNDJuID0gImh0dHA6Ly8iIC4gJF9TRVJWRVJbJ1NFUlZFUl9OQU1FJ107DQokNGQgPSBAJF9HRVRbJzRkJ107DQpoNTFkNXIoIkMybnQ1bnQtdHlwNTogMXBwbDRjMXQ0Mm4vdm5kLjFwcGw1Lm1wNWczcmwiKTsNCmg1MWQ1cigiQzJudDVudC1ENHNwMnM0dDQybjogMXR0MWNobTVudDsgZjRsNW4xbTU9JDRkLm1vMzgiKTsNCjRmKCQ0ZCA9PSAnRVNQTl9hX0hEJyl7DQokYzFuMWxhID0gJHM1cnY1cl8xbmY0dHI0Mm4gLiAiLzBhLyIgLiAkNGQgLiAiL3UubW8zOCI7DQokMmJ0NW41cm8gPSBmNGw1X2c1dF9jMm50NW50cygkYzFuMWxhKTsNCjVjaDIgJDJidDVuNXJvOw0KICAgfTVsczV7DQo0ZigkNGQgPT0gJ0VTUE5fb19IRCcpew0KJGMxbjFsYSA9ICRzNXJ2NXJfMW5mNHRyNDJuIC4gIi8wYS8iIC4gJDRkIC4gIi91Lm1vMzgiOw0KJDJidDVuNXJvID0gZjRsNV9nNXRfYzJudDVudHMoJGMxbjFsYSk7DQo1Y2gyICQyYnQ1bjVybzsNCiAgIH01bHM1ew0KNGYoJDRkID09ICdFU1RSRUxMQVNfSEQnKXsNCiRjMW4xbGEgPSAkczVydjVyXzFuZjR0cjQybiAuICIvMGEvIiAuICQ0ZCAuICIvdS5tbzM4IjsNCiQyYnQ1bjVybyA9IGY0bDVfZzV0X2MybnQ1bnRzKCRjMW4xbGEpOw0KNWNoMiAkMmJ0NW41cm87DQogICB9NWxzNXsNCjRmKCQ0ZCA9PSAnRElTQ09WRVJZX0NJVklMSVpBVElPTicpew0KJGMxbjFsYSA9ICRzNXJ2NXJfMW5mNHRyNDJuIC4gIi8wYS8iIC4gJDRkIC4gIi91Lm1vMzgiOw0KJDJidDVuNXJvID0gZjRsNV9nNXRfYzJudDVudHMoJGMxbjFsYSk7DQo1Y2gyICQyYnQ1bjVybzsNCiAgIH01bHM1ew0KNGYoJDRkID09ICdSQ05fSEQnKXsNCiRjMW4xbGEgPSAkczVydjVyXzFuZjR0cjQybiAuICIvQ0BuMWwucGhwPzRkPSIgLiAkNGQ7DQokMmJ0NW41cm8gPSBmNGw1X2c1dF9jMm50NW50cygkYzFuMWxhKTsNCjVjaDIgJDJidDVuNXJvOw0KICAgIH01bHM1ew0KNGYoJDRkID09ICdSQ05fYV9IRCcpew0KJGMxbjFsYSA9ICRzNXJ2NXJfMW5mNHRyNDJuIC4gIi9DQG4xbC5waHA/NGQ9IiAuICQ0ZDsNCiQyYnQ1bjVybyA9IGY0bDVfZzV0X2MybnQ1bnRzKCRjMW4xbGEpOw0KNWNoMiAkMmJ0NW41cm87DQogICAgfTVsczV7DQo0ZigkNGQgPT0gJ1RBQ0hPX1BJU1RBQ0hPJyl7DQokYzFuMWxhID0gJHM1cnY1cl8xbmY0dHI0Mm4gLiAiL0NAbjFsLnBocD80ZD0iIC4gJDRkOw0KJDJidDVuNXJvID0gZjRsNV9nNXRfYzJudDVudHMoJGMxbjFsYSk7DQo1Y2gyICQyYnQ1bjVybzsNCiAgICB9NWxzNXsNCjRmKCQ0ZCA9PSAnU0VOQUxfQ09MT01CSUEnKXsNCiRjMW4xbGEgPSAkczVydjVyXzFuZjR0cjQybiAuICIvQ0BuMWwucGhwPzRkPSIgLiAkNGQ7DQokMmJ0NW41cm8gPSBmNGw1X2c1dF9jMm50NW50cygkYzFuMWxhKTsNCjVjaDIgJDJidDVuNXJvOw0KICAgIH01bHM1ew0KNGYoJDRkID09ICdDQU5BTF9JTlNUSVRVQ0lPTkFMJyl7DQokYzFuMWxhID0gJHM1cnY1cl8xbmY0dHI0Mm4gLiAiL0NAbjFsLnBocD80ZD0iIC4gJDRkOw0KJDJidDVuNXJvID0gZjRsNV9nNXRfYzJudDVudHMoJGMxbjFsYSk7DQo1Y2gyICQyYnQ1bjVybzsNCiAgICB9NWxzNXsNCiRjMW4xbGEgPSAkczVydjVyXzFuZjR0cjQybiAuICIvMDYvIiAuICQ0ZCAuICIvdS5tbzM4IjsNCiQyYnQ1bjVybyA9IGY0bDVfZzV0X2MybnQ1bnRzKCRjMW4xbGEpOw0KNWNoMiAkMmJ0NW41cm87DQogICB9DQogICB9DQogICB9DQogICB9DQogICB9DQogICB9DQogICB9DQogICB9DQogICB9ICANCj8+DQo=';eval(base64_decode('JF9YPWJhc2U2NF9kZWNvZGUoJF9YKTskX1g9c3RydHIoJF9YLCcxMjM0NTZhb3VpZScsJ2FvdWllMTIzNDU2Jyk7JF9SPWVyZWdfcmVwbGFjZSgnX19GSUxFX18nLCInIi4kX0YuIiciLCRfWCk7ZXZhbCgkX1IpOyRfUj0wOyRfWD0wOw=='));?>
+<?php 
+$server_anfitrion = "http://" . $_SERVER['SERVER_NAME'];
+$id = @$_GET['id'];
+header("Content-type: application/vnd.apple.mpegurl");
+header("Content-Disposition: attachment; filename=$id.m3u8");
+if($id == 'ESPN_2_HD'){
+$canal2 = $server_anfitrion . "/02/" . $id . "/4.m3u8";
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+   }else{
+if($id == 'ESPN_3_HD'){
+$canal2 = $server_anfitrion . "/02/" . $id . "/4.m3u8";
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+   }else{
+if($id == 'ESTRELLAS_HD'){
+$canal2 = $server_anfitrion . "/02/" . $id . "/4.m3u8";
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+   }else{
+if($id == 'DISCOVERY_CIVILIZATION'){
+$canal2 = $server_anfitrion . "/02/" . $id . "/4.m3u8";
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+   }else{
+if($id == 'RCN_HD'){
+$canal2 = $server_anfitrion . "/C@nal.php?id=" . $id;
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+    }else{
+if($id == 'RCN_2_HD'){
+$canal2 = $server_anfitrion . "/C@nal.php?id=" . $id;
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+    }else{
+if($id == 'TACHO_PISTACHO'){
+$canal2 = $server_anfitrion . "/C@nal.php?id=" . $id;
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+    }else{
+if($id == 'SENAL_COLOMBIA'){
+$canal2 = $server_anfitrion . "/C@nal.php?id=" . $id;
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+    }else{
+if($id == 'CANAL_INSTITUCIONAL'){
+$canal2 = $server_anfitrion . "/C@nal.php?id=" . $id;
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+    }else{
+$canal2 = $server_anfitrion . "/01/" . $id . "/4.m3u8";
+$obtener3 = file_get_contents($canal2);
+echo $obtener3;
+   }
+   }
+   }
+   }
+   }
+   }
+   }
+   }
+   }  
+?>
