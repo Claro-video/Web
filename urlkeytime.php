8f35720626ab580e5802d451922fb0084fe4f8b7
vpsradio <45034066+vpsradio@users.noreply.github.com>
2018-12-13 22:14:29 +0100

    Update urlkeytime.php

--- urlkeytime.php
+++ urlkeytime.php
@@ -3,21 +3,21 @@ $urlkey = @$_GET["urlkey"];
 $time = @$_GET["time"];
 
 if($urlkey == 'ESPN_2_HD'){
-	$canal2 = "https://claro-video.github.io/dat/fk/" . $urlkey . ".dat";
+	$canal2 = "https://urlkeytime.pi-dns.tk/fk/" . $urlkey . ".dat";
 	$fp=fopen("$canal2", "r");
 	fpassthru($fp);
    }else{
 if($urlkey == 'ESPN_3_HD'){
-$canal2 = "https://claro-video.github.io/dat/fk/" . $urlkey . ".dat";
+$canal2 = "https://urlkeytime.pi-dns.tk/fk/" . $urlkey . ".dat";
     $fp=fopen("$canal2", "r");
     fpassthru($fp);
    }else{
 if($urlkey == 'ESTRELLAS_HD'){
-	$canal2 = "https://claro-video.github.io/dat/fk/" . $urlkey . ".dat";
+	$canal2 = "https://urlkeytime.pi-dns.tk/fk/" . $urlkey . ".dat";
 	$fp=fopen("$canal2", "r");
 	fpassthru($fp);
    }else{
-	$canal2 = "https://claro-video.github.io/dat/kr/" . $urlkey . "." . $time . ".dat";
+	$canal2 = "https://urlkeytime.pi-dns.tk/kr/" . $urlkey . "." . $time . ".dat";
 	$fp=fopen("$canal2", "r");
 	fpassthru($fp);
    }
