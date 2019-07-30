118ad89a2bf93f17cae54943665f419eec090787
vpsradio <45034066+vpsradio@users.noreply.github.com>
2019-07-30 14:38:27 +0200

    Update Canal.php

--- Canal.php
+++ Canal.php
@@ -44,7 +44,7 @@ $obtener3 = file_get_contents($canal2);
 echo $obtener3;
     }else{
 if($id == 'H2_HD'){
-$canal2 = $server_anfitrion . "/01/" . $id . "/3.m3u8";
+$canal2 = $server_anfitrion . "/hls_kr.php?id=H2_HD&calidad=3";
 $obtener3 = file_get_contents($canal2);
 echo $obtener3;
     }else{
