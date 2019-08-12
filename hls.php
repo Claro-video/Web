ae5063622b9427f1b34729e6394ece03edcf7060
vpsradio <45034066+vpsradio@users.noreply.github.com>
2019-08-12 10:30:02 +0200

    Update hls.php

--- hls.php
+++ hls.php
@@ -2,9 +2,9 @@
 <?php
 date_default_timezone_set('America/Bogota');
 
-$canal = "https://mfwkweb-api.clarovideo.net/services/player/getmedia?api_version=v5.8&authpn=html5player&authpt=ad5565dfgsftr&format=json&region=colombia&device_id=b9ae0d792d48a01f1ee0771e53a5f0df&device_category=web&device_model=html5&device_type=html5&device_so=Firefox&device_manufacturer=windows&HKS=(fdbdd5f4bece461330e2f3c8c5b78051)&preview=0&css=0&device_name=Firefox&crDomain=https://www.clarovideo.com&group_id=" . $alfin . "&stream_type=" . $tipo . ;
+$canal = "https://mfwkweb-api.clarovideo.net/services/player/getmedia?api_version=v5.8&authpn=html5player&authpt=ad5565dfgsftr&format=json&region=colombia&device_id=b9ae0d792d48a01f1ee0771e53a5f0df&device_category=web&device_model=html5&device_type=html5&device_so=Firefox&device_manufacturer=windows&HKS=(fdbdd5f4bece461330e2f3c8c5b78051)&preview=0&css=0&device_name=Firefox&crDomain=https://www.clarovideo.com&group_id=" . $alfin . "&stream_type=hlsprm";
 
-$tipo = @$_GET['tipo'];
+
 
 
 $header =
@@ -22,7 +22,7 @@ $obtener2 = get_data_homs($server2 , $header);
 //header("Content-Disposition: attachment; filename=playlist.m3u8");
 echo $obtener2;
 
-$file = fopen($id_solicitado . "/server.fk", "w");
+$file = fopen($id_solicitado . "/server.kr", "w");
 fputs($file,$obtener2);
 fclose($file);
 
