9f1f7664f94f41feda91af9c1d4bd890db91f4b4
vpsradio <45034066+vpsradio@users.noreply.github.com>
2019-08-26 14:23:45 +0200

    Update hls-test.php

--- hls-test.php
+++ hls-test.php
@@ -4,9 +4,6 @@ date_default_timezone_set('America/Bogota');
 $encodes=@$_GET['encodes'];
 $canal = "https://microfwk-hbopaseo.clarovideo.net/services/player/getmedia?api_version=v5.8&authpn=html5player&authpt=ad5565dfgsftr&format=json&region=colombia&device_id=b9ae0d792d48a01f1ee0771e53a5f0df&device_category=web&device_model=html5&device_type=html5&device_so=Firefox&device_manufacturer=windows&HKS=(fdbdd5f4bece461330e2f3c8c5b78051)&preview=0&css=0&device_name=Firefox&group_id=" . $alfin . "&stream_type=" . $encodes ."&crDomain=https://www.clarovideo.com";
 
-?>
-<?php 
-if($id == 'hls_kr'){
 $header =
 "User-Agent: AndroidDlaApk AndroidDlaApkAccedo\r\n";
 $obtener = get_data_homs($canal , $header);
@@ -22,7 +19,7 @@ $obtener2 = get_data_homs($server2 , $header);
 //header("Content-Disposition: attachment; filename=playlist.m3u8");
 echo $obtener2;
 
-$file = fopen($id_solicitado . "/server.kr.test", "w");
+$file = fopen($id_solicitado . "/server. $encodes", "w");
 fputs($file,$obtener2);
 fclose($file);
 
@@ -35,86 +32,4 @@ $options = array(
 $context = stream_context_create($options);
 $file = file_get_contents($url, false, $context);
 return $file;
-
-   }else{
-if($id == 'hls'){
-$header =
-"User-Agent: AndroidDlaApk AndroidDlaApkAccedo\r\n";
-$obtener = get_data_homs($canal , $header);
-echo $obtener;
-
-
-
-
-$server = json_decode($obtener,TRUE);
-$server2 = $server['response']['media']['video_url'];
-$obtener2 = get_data_homs($server2 , $header);
-//header("Content-type: application/vnd.apple.mpegurl");
-//header("Content-Disposition: attachment; filename=playlist.m3u8");
-echo $obtener2;
-
-$file = fopen($id_solicitado . "/server.fk.test", "w");
-fputs($file,$obtener2);
-fclose($file);
-
-
-function get_data_homs($url, $data){
-$options = array(
-'http'=>array(
-'method'=>"GET",
-'header'=> $data));
-$context = stream_context_create($options);
-$file = file_get_contents($url, false, $context);
-return $file;
-
-
-
-   }else{
-if($id == 'ESTRELLAS_HD3'){
-$canal2 = $server_anfitrion . "/02/" . $id . "/4.m3u8";
-$obtener3 = file_get_contents($canal2);
-echo $obtener3;
-   }else{
-if($id == 'DISCOVERY_CIV3ILIZATION'){
-$canal2 = $server_anfitrion . "/02/" . $id . "/4.m3u8";
-$obtener3 = file_get_contents($canal2);
-echo $obtener3;
-   }else{
-if($id == 'RCNcx_HD3'){
-$canal2 = $server_anfitrion . "/C@nal.php?id=" . $id;
-$obtener3 = file_get_contents($canal2);
-echo $obtener3;
-    }else{
-if($id == 'RCN_2_HD3'){
-$canal2 = $server_anfitrion . "/C@nal.php?id=" . $id;
-$obtener3 = file_get_contents($canal2);
-echo $obtener3;
-    }else{
-if($id == 'TACHO_PIS3TACHO'){
-$canal2 = $server_anfitrion . "/C@nal.php?id=" . $id;
-$obtener3 = file_get_contents($canal2);
-echo $obtener3;
-    }else{
-if($id == 'SEreNAL_CO3LOMBIA'){
-$canal2 = $server_anfitrion . "/C@nal.php?id=" . $id;
-$obtener3 = file_get_contents($canal2);
-echo $obtener3;
-    }else{
-if($id == 'H2_HD3'){
-$canal2 = $server_anfitrion . "/hls_kr.php?id=H2_HD&calidad=3";
-$obtener3 = file_get_contents($canal2);
-echo $obtener3;
-    }else{
-$canal2 = $server_anfitrion . "/01/" . $id . "/4.m3u8";
-$obtener3 = file_get_contents($canal2);
-echo $obtener3;
-   }
-   }
-   }
-   }
-   }
-   }
-   }
-   }
-   }  
-?>
+}
