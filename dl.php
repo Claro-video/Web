4f0e3747cf74069a18e1cab3d9188c13b608aa25
vpsradio <45034066+vpsradio@users.noreply.github.com>
2019-08-26 21:50:37 +0200

    Update dl.php

--- dl.php
+++ dl.php
@@ -9,23 +9,23 @@ $server_ip = $_SERVER['SERVER_ADDR'];
 
 if($id == 'ESPN_2_HD'){
 //$canal2 = $server_anfitrion . "/m3u8.02.php?" . $url . "&ip=". $server_ip . "&exp=". $exp ."&hmac=" . $hmac;
-$canal2 = $server_anfitrion . "/m3u8.02.php?" . $url . "&exp=". $exp ."&hmac=" . $hmac;
+$canal2 = $server_anfitrion . "/m3u8.01.php?" . $url . "&exp=". $exp ."&hmac=" . $hmac;
     $fp=fopen("$canal2", "r");
     fpassthru($fp);
 //echo $obtener3;
    }else{
 if($id == 'ESPN_3_HD'){
-$canal2 = $server_anfitrion . "/m3u8.02.php?" . $url . "&exp=". $exp ."&hmac=" . $hmac;
+$canal2 = $server_anfitrion . "/m3u8.01.php?" . $url . "&exp=". $exp ."&hmac=" . $hmac;
     $fp=fopen("$canal2", "r");
     fpassthru($fp);
    }else{
 if($id == 'ESTRELLAS_HD'){
-$canal2 = $server_anfitrion . "/m3u8.02.php?" . $url . "&exp=". $exp ."&hmac=" . $hmac;
+$canal2 = $server_anfitrion . "/m3u8.01.php?" . $url . "&exp=". $exp ."&hmac=" . $hmac;
     $fp=fopen("$canal2", "r");
     fpassthru($fp);
    }else{
 if($id == 'DISCOVERY_CIVILIZATION'){
-$canal2 = $server_anfitrion . "/m3u8.02.php?" . $url . "&exp=". $exp ."&hmac=" . $hmac;
+$canal2 = $server_anfitrion . "/m3u8.01.php?" . $url . "&exp=". $exp ."&hmac=" . $hmac;
     $fp=fopen("$canal2", "r");
     fpassthru($fp);
    }else{
