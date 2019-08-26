62f4f1f6b52157fcb591503f82f220b154eaa679
vpsradio <45034066+vpsradio@users.noreply.github.com>
2019-08-26 15:19:28 +0200

    Update hls-test.php

--- hls-test.php
+++ hls-test.php
@@ -18,8 +18,8 @@ $obtener2 = get_data_homs($server2 , $header);
 //header("Content-type: application/vnd.apple.mpegurl");
 //header("Content-Disposition: attachment; filename=playlist.m3u8");
 echo $obtener2;
-
-$file = fopen($id_solicitado . "/server.test", "w");
+$carp3= $id_solicitado ."/"$encodes .;
+$file = fopen($carp3 . "/server.test", "w");
 fputs($file,$obtener2);
 fclose($file);
 
