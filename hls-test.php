933152b41daa3cf3cf3d4715ddb1f3d1e6c2e93d
vpsradio <45034066+vpsradio@users.noreply.github.com>
2019-08-26 17:03:22 +0200

    Update hls-test.php

--- hls-test.php
+++ hls-test.php
@@ -1,6 +1,6 @@
 <?php require("ID.php"); ?>
 <?php
-date_default_timezone_set('America/Bogota');
+$date_default_timezone_set('America/Bogota');
 $encodes=@$_GET['encodes'];
 $canal = "https://microfwk-hbopaseo.clarovideo.net/services/player/getmedia?api_version=v5.8&authpn=html5player&authpt=ad5565dfgsftr&format=json&region=colombia&device_id=b9ae0d792d48a01f1ee0771e53a5f0df&device_category=web&device_model=html5&device_type=html5&device_so=Firefox&device_manufacturer=windows&HKS=(fdbdd5f4bece461330e2f3c8c5b78051)&preview=0&css=0&device_name=Firefox&group_id=" . $alfin . "&stream_type=" . $encodes ."&crDomain=https://www.clarovideo.com";
 
@@ -19,7 +19,9 @@ $obtener2 = get_data_homs($server2 , $header);
 //header("Content-Disposition: attachment; filename=playlist.m3u8");
 echo $obtener2;
 $carp3= "$id_solicitado . "/" . $encodes";
-$file = fopen($carp3 . "/server.test", "w");
+// foobar
+echo $id_solicitado,$encodes;     // foobarbarbaz
+$file = fopen($carp3"/server.test", "w");
 fputs($file,$obtener2);
 fclose($file);
 
