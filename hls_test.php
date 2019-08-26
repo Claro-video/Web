722a2632efee851cebd45c7929f7439ff704cbac
vpsradio <45034066+vpsradio@users.noreply.github.com>
2019-08-26 16:17:27 +0200

    Update hls_test.php

--- hls_test.php
+++ hls_test.php
@@ -2,7 +2,7 @@
 
 
 <?php
-$carp3= "$id_solicitado ."/" $encodes";
+$carp3= "$id ."/" $encodes";
 $server_anfitrion = "https://" . $_SERVER['SERVER_NAME'];
 $id = @$_GET['id'];
 $channel=@$_GET['channel'];
@@ -14,7 +14,7 @@ $server_ip = $_SERVER['SERVER_ADDR'];
       $d4=rand(0,255);
 
 
-$canal = file($server_anfitrion. "/" . $id ."/" $encodes "/server.test");
+$canal = file($server_anfitrion. "/" . $carp3 ."/server.test");
 $header =
 "User-Agent:  AndroidDlaApk AndroidDlaApkAccedo\r\n";
 $cali = htmlspecialchars($_GET["calidad"]);
