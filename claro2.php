32f3e2a4130a1fa5f811dad03828aea58c9426cb
vpsradio <45034066+vpsradio@users.noreply.github.com>
2019-12-27 22:08:50 +0100

    Update claro2.php

--- claro2.php
+++ claro2.php
@@ -1,12 +1,12 @@
- <?php
-
-  # llamado del canal - http://localhost/claro.php?calidad=4&canal=***xx&f=.m3u8
- $canal = $_GET['canal'];
- $calidad = $_GET['calidad'];
- $url = 'https://mfwkweb-api.clarovideo.net/services/player/getmedia?api_version=v5.85&authpn=html5player&authpt=ad5565dfgsftr&device_category=web&device_model=html5&device_type=1&device_manufacturer=1&HKS=('.$calidad.')&stream_type=hls_kr&group_id='.$canal;
- $ch = file_get_contents($url);
- $data = json_decode($ch);
- $stream = $data->response->media->video_url;
- header('Location:' .$stream);
- 
- ?> 
+<?php 
+$canal = $_GET['canal'];
+$calidad = $_GET['calidad'];
+$quality = '0'.$calidad;
+$url = 'https://mfwkweb-api.clarovideo.net/services/player/getmedia?api_version=v5.85&authpn=html5player&authpt=ad5565dfgsftr&device_category=web&device_model=html5&device_type=1&device_manufacturer=1&HKS=(1)&stream_type=hls_kr&group_id='.$canal;
+$ch = file_get_contents($url);
+$data = json_decode($ch);
+$stream = $data->response->media->video_url;
+$gstream = str_replace('variant.m3u8', $quality , $stream);
+//$getstream = file_get_contents($stream);
+echo $gstream;
+?>
